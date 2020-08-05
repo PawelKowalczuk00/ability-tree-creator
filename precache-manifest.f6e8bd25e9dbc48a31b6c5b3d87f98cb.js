18e26143ba8d44e668ac887c9c103e2a2c27ccb8
Paweł Kowalczuk <german4050@wp.pl>
2020-08-05 06:24:31 +0200

    Updates

---
+++ precache-manifest.f6e8bd25e9dbc48a31b6c5b3d87f98cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f39d983314745da056f606fdb9a091d",
+    "url": "/ability-tree-creator/index.html"
+  },
+  {
+    "revision": "1c6cd4bc1fc64178113d",
+    "url": "/ability-tree-creator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8d2d471b2d7b89c826d6",
+    "url": "/ability-tree-creator/static/js/2.ca8cdd8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ability-tree-creator/static/js/2.ca8cdd8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c6cd4bc1fc64178113d",
+    "url": "/ability-tree-creator/static/js/main.6c26bfff.chunk.js"
+  },
+  {
+    "revision": "9dfb4f202f8e6ccd643d",
+    "url": "/ability-tree-creator/static/js/runtime-main.3f864328.js"
+  }
+]);
